23faf94c49477eb6ba03af4228ae87a74273434f
vicksoson <okon.victor.u@gmail.com>
2021-12-02 22:24:38 +0100

    fix

--- app/src/main/java/com/example/maipady/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/maipady/ui/home/HomeFragment.kt
@@ -8,10 +8,22 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.maipady.R
 import com.example.maipady.databinding.FragmentHomeBinding
+import com.google.android.material.appbar.AppBarLayout
+
+var animationPlaybackSpeed: Double = 0.8
 
 class HomeFragment : Fragment() {
+
+
+//    private val recyclerView: RecyclerView by bindView(R.id.recycler_view)
+//    private val appbar: AppBarLayout by bindView(R.id.appbar)
+//    private val drawerIcon: View by bindView(R.id.drawer_icon)
+//    private val filtersLayout: FiltersLayout by bindView(R.id.filters_layout)
+//    private val filtersMotionLayout: FiltersMotionLayout by bindView(R.id.filters_motion_layout)
+
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
